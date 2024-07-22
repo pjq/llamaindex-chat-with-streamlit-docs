2f59e4070f852081ca2f2109683813177d63b6be
Jianqing Peng <Jianqing.Peng@sap.com>
2024-07-22 15:24:43 +0800

    update llm,base_url

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,31 +3,32 @@ import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with the MDC/PPX docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
+openai.base_url= st.secrets.base_url
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.info("Check out the full MDC tutorial [blog post](https://pages.github.tools.sap/sfmobile/mdc-docs/)", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
+            "content": "Ask me a question about SuccessFactors MDC document!",
         }
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+    reader = SimpleDirectoryReader(input_dir="../mdc-docs/docs/", recursive=True)
     docs = reader.load_data()
     Settings.llm = OpenAI(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         temperature=0.2,
         system_prompt="""You are an expert on 
-        the Streamlit Python library and your 
+        the SAP SuccessFactors Mobile MDC(Metadata driven control) and your 
         job is to answer technical questions. 
         Assume that all questions are related 
-        to the Streamlit Python library. Keep 
+        to the MDC. Keep 
         your answers technical and based on 
         facts – do not hallucinate features.""",
     )
